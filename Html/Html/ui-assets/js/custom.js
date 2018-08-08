0b8c652f6d98cacc84eb638144d793beefebff35
amaljithUid <amaljith82@gmail.com>
2018-08-08 10:24:21 +0530

    new updated

--- Html/Html/ui-assets/js/custom.js
+++ Html/Html/ui-assets/js/custom.js
@@ -1,10 +1,20 @@
 $.fn.hoverBlock = function() {
     // console.log('open');
+
     $(this).on('mouseover', function(){
         $(this).siblings().removeClass("open");
         $(this).addClass('open');
+        $(this).children('').addClass('enebled');
         console.log(this);
     });
+
+    $(this).on('mouseleave', function(){
+        if($(this).children().not(".enebled")){
+            $(this).removeClass('open');
+        }
+    });
+
+    //if($(this).hasClass("enebled"))
     
 }
 
